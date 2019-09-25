fe2dc870a3c2ea54bfd131363dd8e7a4cac17c6b
Cristian Cordoba <cristiandc10@live.com>
2019-09-25 03:08:15 -0400

    Update read-in-info.js

--- js/read-in-info.js
+++ js/read-in-info.js
@@ -14,14 +14,14 @@ let project4Captions = [];
 let projectCaptions = [project1Captions, project2Captions, project3Captions, project4Captions];
 
 for (let i = 1; i <= 4; i++) {
-    $.get('img/project-images/project' + i + '/', function (data) {
+    $.get('/img/project-images/project' + i + '/', function (data) {
         $(data).find("a:contains('.png'), a:contains('.PNG')").each(function () {
             projectImages[i - 1].push($(this).attr("href"));
             console.log($(this).attr("href"));
         });
     });
 
-    $.get('img/project-images/project' + i + '/Captions.txt', function (data) {
+    $.get('/img/project-images/project' + i + '/Captions.txt', function (data) {
         data.split("\n").forEach(function (sentence) {
             projectCaptions[i - 1].push(sentence);
         });
